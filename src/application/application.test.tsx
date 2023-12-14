3decb31121803a4a143d16f062cf199315113c7e
Creakyinertia <darshanvgowda1813@gmail.com>
2023-12-14 17:45:06 +0530

    'firstcommit'

---
+++ src/application/application.test.tsx
@@ -0,0 +1,31 @@
+import { render,screen } from "@testing-library/react";
+import { Application } from "./application";
+
+test("Render elements",()=>{
+    render(<Application/>);
+    const textElement = screen.getByRole("textbox",
+    {name:'Name'}
+    );
+    expect(textElement).toBeInTheDocument();
+
+    const placeholderElement = screen.getByPlaceholderText("name");
+    expect(placeholderElement).toBeInTheDocument();
+
+    const DescElement = screen.getByRole("textbox",
+    {name:'description'}
+    );
+    expect(DescElement).toBeInTheDocument();
+
+    const paratextElement = screen.getByText("All fields are mandatory");
+    expect(paratextElement).toBeInTheDocument();
+
+
+    const comboboxElement = screen.getByRole("combobox");
+    expect(comboboxElement).toBeInTheDocument();
+
+    const checkboxElement = screen.getByRole("checkbox");
+    expect(checkboxElement).toBeInTheDocument();
+
+    const buttonElement = screen.getByRole("button");
+    expect(buttonElement).toBeInTheDocument();
+})
---
+++ src/application/application.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export const Application = () => {
+  return (
+    <>
+        <div>
+            <label htmlFor='name'>Name</label>
+            <input id='name' type='text' placeholder='name' />
+        </div>
+        <p>All fields are mandatory</p>
+        <div>
+            <label htmlFor='desc'>description</label>
+            <textarea id='desc'/>
+        </div>
+        <div>
+            <label htmlFor='job-location'>job-location</label>
+            <select id='job-location'>
+                <option value='New York'>New York</option>
+                <option value='Los Angeles'>Los Angeles</option>
+                <option value='Chicago'>Chicago</option>
+            </select>
+        </div>
+        <div>
+            <input type='checkbox' id='terms'/>Accept the terms and conditions
+        </div>
+        <button type='submit'>Submit</button>
+    </>
+  )
+}
